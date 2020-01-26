c3a29d26190902cc38a97f7b2c27b77b5368603e
Tuemo <thomastuemo@yahoo.fr>
2020-01-26 18:29:03 +0100

    premier commit

---
+++ src/main/java/com/ipsas/gi/entities/Product.java
@@ -0,0 +1,60 @@
+package com.ipsas.gi.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int code;
+	private String name;
+	private int prix;
+	public Product() {
+		
+		
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Product(int code, String name, int prix) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.prix = prix;
+	}
+	
+	
+	
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrix() {
+		return prix;
+	}
+	
+	
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+	@Override
+	public String toString() {
+		return "Product [code=" + code + ", name=" + name + ", prix=" + prix + "]";
+	}
+	
+
+}
